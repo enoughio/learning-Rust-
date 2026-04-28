aa563199f97148312ab30152a22196cc5a0a6e13
aniket <aniketjatav.dev@gmail.com>
2026-04-28 23:54:10 +0530

    understand rust crate managment

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,22 +1,21 @@
-use std:id
+use std::io;
 
 
 fn main() {
 
-    println!("guess the number")
-    println!("Enter your guess : ")
+    println!("guess the number =---------= : ");
+    println!("");
+    println!("Enter your guess : ");
 
     let mut guess = String::new();   // creates a new empty instace of a string 
     
+    // getting an input from user 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read the line");
+        .expect("Failed to read input");
     
+    println!("Your guess {}" ,guess);
+
     
-
-
-
-
-
 
 }
